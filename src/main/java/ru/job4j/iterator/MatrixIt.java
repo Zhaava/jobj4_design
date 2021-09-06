6d1fe84d3cc2bc5c2bd2bed0292f23bba564c925
Alexey <zhavoronkov52@gmail.com>
2021-09-06 19:00:57 +0300

    5.1.4. FlatMap для Iterator<Iterator> [#152]

--- src/main/java/ru/job4j/iterator/MatrixIt.java
+++ src/main/java/ru/job4j/iterator/MatrixIt.java
@@ -4,7 +4,6 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class MatrixIt implements Iterator<Integer> {
-
     private final int[][] data;
     private int row = 0;
     private int column = 0;
